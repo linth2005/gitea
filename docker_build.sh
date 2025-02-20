62d33e68a56dbd3910c237a105b9b39a1c8c1144
eternal <wangchenboy@163.com>
2025-02-20 23:43:14 +0800

    fix: update docker image tag to v1.23.4

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,4 @@
 # 基本构建命令 docker build -t [私有仓库地址]/[命名空间]/[镜像名]:[标签] .
-docker build --build-arg GOPROXY=https://goproxy.cn -t nexus.fhatt.cn/qf-gitea:v1.23.1 .
+docker build --build-arg GOPROXY=https://goproxy.cn -t nexus.fhatt.cn/qf-gitea:v1.23.4 .
 
-docker push exus.fhatt.cn/qf-gitea:v1.23.1
+docker push exus.fhatt.cn/qf-gitea:v1.23.4
